7fa9cda2ca2bf65985c120e334f402d3f913b37b
kipman725 <kipman725@kipman725.(none)>
2011-11-22 20:17:15 +0000

    support for emotion passing not sorted yet

---
+++ rosfred/speech_synth/README.txt
@@ -0,0 +1,1 @@
+This speech synth is basicly a ROS interface to espeak with addition emotion support via parameter calling  
---
+++ rosfred/speech_synth/build/CMakeFiles/speech_synth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/speech_synth.dir/src/speech_synth.o"
+  "../bin/speech_synth.pdb"
+  "../bin/speech_synth"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/speech_synth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
