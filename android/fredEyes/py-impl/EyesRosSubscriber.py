475c124dc3d71a7c58430ca9614f291ef7d5c0e7
Yiqi <adelineyiqi@googlemail.com>
2011-11-29 21:45:27 +0000

    ros node in the py file

--- android/fredEyes/py-impl/EyesRosSubscriber.py
+++ android/fredEyes/py-impl/EyesRosSubscriber.py
@@ -10,36 +10,35 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-try:
-    # Make socket
-    transport = TSocket.TSocket(sys.argv[1], 9090)
-    
-    # Buffering is critical. Raw sockets are very slow
-    transport = TTransport.TBufferedTransport(transport)
-    
-    # Wrap in a protocol
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-    
-    # Create a client to use the protocol encoder
-    client = FredEyes.Client(protocol)
-    
-    # Connect!
-    transport.open()
-    
-    
-    def listener():
+def listener():
+    try:
+        # Make socket
+        transport = TSocket.TSocket(sys.argv[1], 9090)
+
+        # Buffering is critical. Raw sockets are very slow
+        transport = TTransport.TBufferedTransport(transport)
+
+        # Wrap in a protocol
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+        # Create a client to use the protocol encoder
+        client = FredEyes.Client(protocol)
+
+        # Connect!
+        transport.open()
+        
+        # ROS code
         rospy.init_node('FredEyes', anonymous=True)
         rospy.Subscriber("setHappiness", int , client.setHappiness)
         rospy.Subscriber("setFocus", Location , client.setFocus)
         rospy.spin()
     
-    if __name__ == '__main__':
+    except Thrift.TException, tx:
+        print "%s" % (tx.message)
+    except IndexError, e:
+        print "Usage: python FredEyesClient.py [ip_address]/n"
+    finally:
+        transport.close()
+
+if __name__ == '__main__':
     listener()
-
-except Thrift.TException, tx:
-    print "%s" % (tx.message)
-
-except IndexError, e:
-    print "Usage: python FredEyesClient.py [ip_address]/n"
-finally:
-    transport.close()
