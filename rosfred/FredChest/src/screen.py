81d0dbde44497b7bbf435e5a87ed5ac9f442207f
Valdas K <elatier@gmail.com>
2011-12-02 15:58:51 +0000

    Added support to show different images when object detected

--- rosfred/FredChest/src/screen.py
+++ rosfred/FredChest/src/screen.py
@@ -4,13 +4,16 @@ import rospy
 import pygame.display
 import pygame.image
 from Planner.msg import Mood
+from Planner.msg import Bottle
 from std_msgs.msg import String
 import os
 
+bottle_pic = pygame.image.load("sign.png")
+picture = bottle_pic
 
 def callbackscreen(message):
 	if message.data == "full":
-		screenplane = pygame.display.set_mode((0,0),pygame.FULLSCREEN,0)
+		screenplane = pygame.display.set_mode((0,0),pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE,0)
 		screenplane.blit(picture, (0, 0))
 		pygame.display.update()
 		rospy.loginfo(rospy.get_name()+"Switching to fullscreen mode")
@@ -30,18 +33,26 @@ def callback(message):
     screenplane.blit(picture, (0, 0))
     pygame.display.update()
 
+def objectDetected(msg):
+	global picture
+	if msg.obj_detected == 0:
+		rospy.loginfo(rospy.get_name()+"Detected not bottle")
+	elif msg.obj_detected == 1:
+		rospy.loginfo(rospy.get_name()+"Bottle detected")
+
 def screen():
-    rospy.init_node('screen')
-    rospy.Subscriber("mood", Mood, callback)
-    rospy.Subscriber("screen",String, callbackscreen)
-    rospy.spin()
+	rospy.init_node('screen')
+	rospy.Subscriber("mood", Mood, callback)
+	rospy.Subscriber("screen",String, callbackscreen)
+	#rospy.Subscriber("bottle", Bottle, objectDetected)
+	rospy.spin()
 
 if __name__ == '__main__':
 	pygame.display.init()
-	print os.getcwd()
-	picture = pygame.image.load("sign.png")
+	#current path of the screen.py
+	print "current path of the screen.py"+os.getcwd()
 	#screen resolution: (0,0) is current, flags: pygame.FULLSCREEN for fullscreen, depth.
-	screenplane = pygame.display.set_mode((0,0),pygame.FULLSCREEN,0)
+	screenplane = pygame.display.set_mode((0,0),pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE,0)
 	screenplane.blit(picture, (0, 0))
 	pygame.display.update()
 	screen()
