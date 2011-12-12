80625f36dc3f611926a48729f4eb066f16ce83d4
fredbot <fredbot@fredbot-desktop.(none)>
2011-12-12 23:03:33 +0000

    Made thrift reconnect on drop of connection

--- rosfred/FredEyes/nodes/EyesRosSubscriber.py
+++ rosfred/FredEyes/nodes/EyesRosSubscriber.py
@@ -17,50 +17,51 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-    
+	
 class Listener():
-    def run(self):
-        try:
-            transport = TSocket.TSocket(rospy.get_param("/FredEyes/tablet_ip"), 9090)
-            try:
-                # Buffering is critical. Raw sockets are very slow
-                transport = TTransport.TBufferedTransport(transport)
+	def connect(self):
+		transport = TSocket.TSocket(rospy.get_param("/FredEyes/tablet_ip"), 9090)
+		try:
+			# Buffering is critical. Raw sockets are very slow
+			transport = TTransport.TBufferedTransport(transport)
 
-                # Wrap in a protocol
-                protocol = TBinaryProtocol.TBinaryProtocol(transport)
+			# Wrap in a protocol
+			protocol = TBinaryProtocol.TBinaryProtocol(transport)
 
-                # Create a client to use the protocol encoder
-                self.client = FredEyes.Client(protocol)
+			# Create a client to use the protocol encoder
+			self.client = FredEyes.Client(protocol)
 
-                # Connect!
-                transport.open()
-                
-        ################### ROS code ######################################
-                rospy.init_node('FredEyes', anonymous=True)
-                rospy.Subscriber("setHappiness", Int32, self.setHappiness, queue_size=1)
-                rospy.Subscriber("setFocus", Vector3, self.setFocus, queue_size=1)
-                print "Spinning!"
-                rospy.spin() 
-        ##################################################################
+			# Connect!
+			transport.open()
 
-            except Thrift.TException, tx:
-                print "%s" % (tx.message)
-            finally:
-		print "Problem!"
-                transport.close()
+		except Thrift.TException, tx:
+			print "%s" % (tx.message)
+		
+	def run(self):
+		if(not hasattr(self, "client")):
+			self.connect()
 
-        except IndexError, e:
-            print "Usage: python FredEyesClient.py [ip_address]\n"
+		rospy.init_node('FredEyes', anonymous=True)
+		rospy.Subscriber("setHappiness", Int32, self.setHappiness, queue_size=1)
+		rospy.Subscriber("setFocus", Vector3, self.setFocus, queue_size=1)
+		print "Spinning!"
+		rospy.spin() 
 
-    def setFocus(self, data):
-        loc = Location(data.x, data.y, data.z)
-        self.client.setFocus(loc)
+	def setFocus(self, data):
+		loc = Location(data.x, data.y, data.z)
+		try:
+			self.client.setFocus(loc)
+		except:
+			self.connect()
 
-    def setHappiness(self, data):
-        happiness = data.data
-        self.client.setHappiness(happiness)
+	def setHappiness(self, data):
+		happiness = data.data
+		try:
+			self.client.setHappiness(happiness)
+		except:
+			self.connect()
 
 if __name__ == '__main__':
-    l = Listener()
-    l.run()
+	l = Listener()
+	l.run()
 
