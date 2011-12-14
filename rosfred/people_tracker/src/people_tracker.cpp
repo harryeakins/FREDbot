b42cc4bfcaca802a3bf496cbd23953a31cad6826
fredbot <fredbot@fredbot-desktop.(none)>
2011-12-14 13:49:24 +0000

    Polar coordinates in people_tracker and removed binary

--- rosfred/people_tracker/src/people_tracker.cpp
+++ rosfred/people_tracker/src/people_tracker.cpp
@@ -6,13 +6,14 @@
 #include <std_msgs/String.h>
 #include <std_msgs/Int32.h>
 #include <geometry_msgs/Vector3.h>
-
+#include <people_tracker/personLocation.h>
 #include <std_msgs/Header.h>
 #include "Person.h"
 #include <string>
 #include "KinectController.h"
-
+#include <math.h>
 #include <ros/console.h>
+#define PI 3.14159265
 
 using std::string;
 
@@ -32,11 +33,13 @@ namespace people_tracker
 		n.param("tracking_rate", rate, 1);
 		closest_person_pub = n.advertise<geometry_msgs::Vector3>("/closestPerson", 1000);
 		people_count_pub = n.advertise<std_msgs::Int32>("/numPeople", 1000);
+		person_angle_pub = n.advertise<std_msgs::Int32>("/personAngle", 1000);
+		person_location_pub = n.advertise<people_tracker::personLocation>("/personLocation", 1000);
       }
       
 
 //Contains loop for tracking people
-    void processKinect(KinectController &kinect_controller)
+    void processKinect(KinectController &kinect_controller, int kinectDistance)
       {
         XnUserID users[15];
         XnUInt16 users_count = 15;
@@ -53,8 +56,8 @@ namespace people_tracker
 
 	//People Topic Variables
 	geometry_msgs::Vector3 closestMsg;
-	std_msgs::Int32 countMsg;
-	
+	std_msgs::Int32 countMsg, angleMsg;
+	people_tracker::personLocation personLocMsg;
 	
 	//ROS_INFO("%s", msg.data.c_str());
 	
@@ -80,12 +83,35 @@ namespace people_tracker
 	if (userCount>0){
 		
 		UserGenerator.GetCoM(closestUser, com);
-		closestMsg.x = com.X;
+		
+		float theta, realDistance, r;
+		realDistance = com.Z - kinectDistance;
+
+
+		theta = atan(com.X/(realDistance));
+
+		r = realDistance/cos(theta);
+
+		theta = (realDistance/abs(realDistance))*theta;
+		//r is the actual real distance
+		
+		angleMsg.data = (int)(180*theta/PI);
+
+		person_angle_pub.publish(angleMsg);
+		
+		personLocMsg.Angle = (int)(180*theta/PI);
+		personLocMsg.Distance = r;
+		
+
+		person_location_pub.publish(personLocMsg);
+		//ROS_ERROR("angle: %f", com.Z);
+		//a2 = b2 + c2 - 2bc cosA
+		
+		closestMsg.x = (int)(1300*theta/PI);
 		closestMsg.y = com.Y;
 		closestMsg.z = com.Z;
 		
 		closest_person_pub.publish(closestMsg);
-		
 		
 	}
 	countMsg.data = userCount;
@@ -93,7 +119,7 @@ namespace people_tracker
       }
          
   private:
-    ros::Publisher  closest_person_pub, people_count_pub ;
+    ros::Publisher  closest_person_pub, people_count_pub, person_angle_pub, person_location_pub ;
     
     
   };
@@ -108,7 +134,9 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "people_tracker");
   ros::NodeHandle np("~");
-
+  int distance;
+  if (!np.getParam("kinect_distance",distance)) ROS_ERROR("No kinect distance param");
+ 
   string filepath;
   np.getParam("load_filepath", filepath); 
   ROS_WARN_STREAM("Filepath: " << filepath);
@@ -120,7 +148,7 @@ int main(int argc, char** argv)
 	while(ros::ok()){
 		xn::SceneMetaData sceneMD;
   		g_kinect_controller.getContext().WaitAndUpdateAll();
-  		g_people_tracker.processKinect(g_kinect_controller);
+  		g_people_tracker.processKinect(g_kinect_controller, distance);
   		g_kinect_controller.getUserGenerator().GetUserPixels(0, sceneMD);  
 	
 	}
