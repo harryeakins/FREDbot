91ec552e85fd3da33c246f918563fd74916cc1ce
fredbot <fredbot@fredbot-desktop.(none)>
2011-12-15 12:41:46 +0000

    Changed speech_synth to accept mood topics and strings messages instead of emotion strings

--- rosfred/speech_synth/src/speech_synth.cpp
+++ rosfred/speech_synth/src/speech_synth.cpp
@@ -1,12 +1,22 @@
 #include "ros/ros.h"
 #include "espk.h" //espk based voice synth
-#include "emote_text.h" //custom message type
+//#include "emote_text.h" //custom message type
+#include "std_msgs/String.h"
+#include "std_msgs/Int32.h"
+
+int mood =  0;
 
 //The speech synthersiser!
-void chatterCallback(const speech_synth::emote_text::ConstPtr& msg)
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s] with emotion [%f]", msg->text.c_str(), msg->emotion);
-  espk(msg->emotion, msg->text.c_str());	
+  ROS_INFO("I heard: [%s] with emotion [%i]", msg->data.c_str(), mood);
+  espk((float)mood/20, msg->data.c_str());	
+}
+
+//mood topic
+void moodCallback(const std_msgs::Int32::ConstPtr& msg)
+{
+  mood = msg->data;	
 }
 
 int main(int argc, char **argv)
@@ -14,6 +24,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "speech_synth");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::Subscriber sub1 = n.subscribe("mood", 1000, moodCallback);
   ros::spin();
   return 0;
 }
